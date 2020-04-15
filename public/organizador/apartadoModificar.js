6994d9d0a28d7ca3680c51b3f0b6698e62868045
Joalbert Milano <joalbertmilano@gmail.com>
2020-04-14 20:42:54 -0400

    añadiendo apartado de modificar

---
+++ public/organizador/apartadoModificar.js
@@ -0,0 +1,6 @@
+function mostrarModificarTarea(){
+    document.getElementById("modificandoTarea").style.display="block";
+}
+function mostrarModificarEvento(){
+    document.getElementById("modificandoEvento").style.display="block";
+}
